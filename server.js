d5bdb6df4e27812ba539af2375c673023e008ffd
Astrid Ge <qiutongg@umich.edu>
2024-10-02 01:33:34 -0700

    complete sign in page

--- server.js
+++ server.js
@@ -64,10 +64,6 @@ app.get("/auth/sign-up", (req,res) => {
     res.render("auth/sign-up.ejs")
 })
 
-app.get("/auth/sign-up", (req,res) => {
-    res.render("auth/sign-up.ejs")
-})
-
 
 app.post("/auth/sign-up", async (req,res) => {
     try {
@@ -94,6 +90,39 @@ app.post("/auth/sign-up", async (req,res) => {
         console.log(error);
         res.redirect('/');
       }
+})
+
+app.post("/auth/sign-in", async (req,res) => {
+    try {
+        // First, get the user from the database
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (!userInDatabase) {
+          return res.send('Login failed. Please try again.');
+        }
+      
+        // There is a user! Time to test their password with bcrypt
+        const validPassword = bcrypt.compareSync(
+          req.body.password,
+          userInDatabase.password
+        );
+        if (!validPassword) {
+          return res.send('Login failed. Please try again.');
+        }
+      
+        // There is a user AND they had the correct password. Time to make a session!
+        // Avoid storing the password, even in hashed format, in the session
+        // If there is other data you want to save to `req.session.user`, do so here!
+        req.session.user = {
+          username: userInDatabase.username,
+          _id: userInDatabase._id
+        };
+      
+        res.redirect('/');
+      } catch (error) {
+        console.log(error);
+        res.redirect('/');
+      }
+
 })
 
 
