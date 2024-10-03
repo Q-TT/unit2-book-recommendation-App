bbadde668361248181029109813f108c77226755
Astrid Ge <qiutongg@umich.edu>
2024-10-03 11:00:07 -0700

    complete the add a new book featre

--- controllers/bookapp.js
+++ controllers/bookapp.js
@@ -32,5 +32,23 @@ router.get('/new', async (req, res) => {
       }
   });
 
+router.post('/new', async(req,res) => {
+    try {
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Push req.body (the new form data object) to the
+        // applications array of the current user
+        currentUser.books.push(req.body);
+        // Save changes to the user
+        await currentUser.save();
+        // Redirect back to the applications index view
+        res.redirect(`/users/${currentUser._id}/bookapp`);
+      } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/')
+      }
+
+})
 
 module.exports = router;
